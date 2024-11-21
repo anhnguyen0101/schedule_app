89ca760ea3dcde6038d1acf4436efa941930d097
Thi Van Anh Nguyen <AnhThiVanNguyen@my.unt.edu>
2024-11-21 11:49:15 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+"use client"
+import './tailwind.css';
+import {
+  Day,
+  Inject,
+  Month,
+  ScheduleComponent,
+  ViewDirective,
+  ViewsDirective,
+  Week
+} from "@syncfusion/ej2-react-schedule";
+import { registerLicense } from "@syncfusion/ej2-base";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+registerLicense(
+  "Ngo9BigBOggjHTQxAR8/V1NDaF5cWWtCf1FpRGBGfV5ycEVAal5UTnNWUj0eQnxTdEFiWH5dcnZURmJfWUd+XQ=="
+);
+
+
+//can add data from json
+const data = [
+  {
+    Id: 1,
+    Subject: "Sales Presentation",
+    StartTime: new Date(2024, 10, 11, 10, 0),
+    EndTime: new Date(2024, 10, 11, 12, 30),
+    IsAllDay: false,
+  },
+  {
+    Id: 2,
+    Subject: "New Budget Report",
+    StartTime: new Date(2024, 10, 10, 10, 0),
+    EndTime: new Date(2024, 10, 10, 12, 30),
+    IsAllDay: true,
+    Status: "Completed",
+    Priority: "High",
+  },
+];
+
+
+export default function Page() {
+    return (
+      <main className="flex justify-center">
+        <ScheduleComponent
+          width="100%"
+          height={600}
+          eventSettings={{ dataSource: data }}
+          //selectedDate={new Date(2025, 1, 11)}  //default date is curr month, changes default to selected day
+          //currentView="Month"   //default view is week, this changes default view to month
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+          <ViewsDirective>
+            <ViewDirective option="Day" />
+            <ViewDirective option="Week" />
+            <ViewDirective option="Month" />
+          </ViewsDirective>
+  
+  
+          <Inject services={[Day, Week, Month]} />
+        </ScheduleComponent>
+      </main>
+    );
+  }
+  
